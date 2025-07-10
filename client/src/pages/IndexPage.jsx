5fe4a8192362a2311cb52bc7671b9260b6550b0e
Doug <leedke@gmail.com>
2025-07-09 19:11:04 -0500

    added index, login, and registration page, basic routing

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const IndexPage = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to OpenChat</h1>
+      <p>This is the main page!</p>
+    </div>
+  );
+};
+
+export default IndexPage;
---
+++ client/src/pages/RegistrationPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RegistrationPage = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Registration Page</h1>
+      <p>This is the registration page!</p>
+    </div>
+  );
+};
+
+export default RegistrationPage;
