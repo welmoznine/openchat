fbd2ff2e2dc4521831bd047555d207aa344aba19
Archie McQuown <archie.mcquown@gmail.com>
2025-07-12 08:34:31 -0700

    added chat UI page and route to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
+import ChatPage from './pages/ChatPage'
 
 import './App.css';
 
@@ -13,7 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/chat" element={<ChatPage />} />
         <Route path="/register" element={<RegistrationPage />} />
+        
       </Routes>
     </Router>
   )
