bf7f13794b76bca33af0c1199ef7ad66ac165da2
Doug <leedke@gmail.com>
2025-07-12 10:55:29 -0500

    removed auth console.log, added root folder db scripts

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -18,7 +18,6 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
 
     const { email, password } = req.body;
-    console.log(email, password);
 
     try {
 
