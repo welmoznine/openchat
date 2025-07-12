401cde3286a2f93b7ea7e8196f3524fee78c8890
Doug <leedke@gmail.com>
2025-07-12 13:51:45 -0500

    fixed linting error, removed user hook from index until test adjusted

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -2,16 +2,10 @@ import { useCurrentUser } from "../hooks/auth/useCurrentUser";
 
 const IndexPage = () => {
 
-  const { user, loading } = useCurrentUser();
-
-  if (loading) return null;
-  if (!user) return null;
-
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
       <h1>Welcome to OpenChat</h1>
       <p>This is the main page!</p>
-      <p>{user.username}</p>
     </div>
   );
 };
