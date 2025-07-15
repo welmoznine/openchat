2aa2f6d72aca22dc8093aa46d91cedf7fdde2593
Doug <leedke@gmail.com>
2025-07-15 18:55:03 -0500

    modified /login test to account for page load

--- client/src/__tests__/App.test.jsx
+++ client/src/__tests__/App.test.jsx
@@ -3,10 +3,10 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 describe('App', () => {
-  it('renders the login page at root path for unauthenticated user', () => {
+  it('renders the login page at root path for unauthenticated user', async () => {
     render(<App />)
     
-    const heading = screen.getByRole('heading', { name: /welcome to openchat/i })
+    const heading = await screen.findByRole('heading', { name: /welcome to openchat/i })
     expect(heading).toBeInTheDocument()
     
   })
