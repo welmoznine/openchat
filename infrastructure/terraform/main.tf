4322857d36add37e8eebffbcd7f1813e72f4b367
yshrkume <yshrkume@gmail.com>
2025-07-26 21:16:04 +0900

    fix: ignore client and client_version changes in terraform cloud run services

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -318,6 +318,8 @@ resource "google_cloud_run_v2_service" "frontend" {
   lifecycle {
     ignore_changes = [
       template[0].containers[0].image,
+      client,
+      client_version,
     ]
   }
 
@@ -402,6 +404,8 @@ resource "google_cloud_run_v2_service" "backend" {
   lifecycle {
     ignore_changes = [
       template[0].containers[0].image,
+      client,
+      client_version,
     ]
   }
 
