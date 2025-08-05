f5772ac1385dce0862be996a0dc02e44be83d115
yshrkume <yshrkume@gmail.com>
2025-08-05 17:10:05 +0900

    fix: remove updatedAt field from Message model

--- server/prisma/migrations/20250804013913_added_delete_tracking_to_message/migration.sql
+++ server/prisma/migrations/20250804013913_added_delete_tracking_to_message/migration.sql
@@ -1,13 +1,6 @@
-/*
-  Warnings:
-
-  - Added the required column `updated_at` to the `Messages` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "Messages" ADD COLUMN     "deleted_at" TIMESTAMP(3),
-ADD COLUMN     "is_deleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+ADD COLUMN     "is_deleted" BOOLEAN NOT NULL DEFAULT false;
 
 -- CreateIndex
 CREATE INDEX "Messages_is_deleted_idx" ON "Messages"("is_deleted");
